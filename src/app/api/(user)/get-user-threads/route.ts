e071e76c9a7ad75e043c02c921ccd4a26e78a09b
meistaaa <alimuhammad9868@gmail.com>
2024-11-29 22:44:49 +0500

    get-user-threads

--- src/app/api/(user)/get-user-threads/route.ts
+++ src/app/api/(user)/get-user-threads/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { getServerSession, User } from "next-auth";
 import { authOptions } from "../../(authentication)/auth/[...nextauth]/options";
+import UserModel from "@/app/models/User";
 export async function GET(req: NextRequest) {
   await dbConnect();
 
@@ -32,13 +33,33 @@ export async function GET(req: NextRequest) {
     }
     const threads = await Thread.find(query)
       .sort({ createdAt: -1 })
-      .limit(limit);
+      .limit(limit)
+      .populate("author", "username avatar"); // Populate user information
+
+    const threadsWithUser = await Promise.all(
+      threads.map(async (thread) => {
+        const user = await UserModel.findById(thread.userId);
+        return {
+          ...thread.toObject(),
+          user: user
+            ? {
+                _id: user._id,
+                username: user.username,
+                avatar: user.avatar,
+              }
+            : null,
+        };
+      })
+    );
+
     return NextResponse.json(
       {
         success: true,
-        data: threads,
+        data: threadsWithUser,
         nextCursor:
-          threads.length > 0 ? threads[threads.length - 1].createdAt : null,
+          threads.length > 0
+            ? threads[threads.length - 1].createdAt.toISOString()
+            : null,
       },
       { status: 200 }
     );
